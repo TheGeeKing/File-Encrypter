26fd3ee1d8c2010cf0d409df5458557a6ddc2620
TheGeeKing <58857539+TheGeeKing@users.noreply.github.com>
2022-03-29 20:42:38 +0100

    Update file_encrypter.py
    - Fixed: app is now in only in dark mode
    - Fixed: verify that path is a file
    - Fixed: size of the app is scaled on computer screen width
    - Fixed: if file has a UnicodeDecodeError while encrypt_file() it is now handled
    - Added: popup_error if Key is incorrect
    - Added: resize of the Multiline when window is maximized
    - Changed: every elements are now centered
    - Changed: Multiline is now 5 lines height when small
    - Changed: 'Cancel' button is now called 'Close'

--- file_encrypter.py
+++ file_encrypter.py
@@ -1,9 +1,11 @@
 import base64
 import hashlib
+from os.path import isfile
 
 import PySimpleGUI as sg
 from Crypto import Random
 from Crypto.Cipher import AES
+from win32api import GetSystemMetrics
 
 
 class AESCipher(object):
@@ -95,24 +97,47 @@ def encrypt_file(file_path, content, key=''):
     with open(file_path, 'w', encoding='utf-8') as f:
         f.write(encrypted)
 
+sg.theme('DarkAmber')
+layout = [[sg.Text('Select a file:'), sg.Input(key='-INPUT-'), sg.FileBrowse()],
+          [sg.Text('Encryption key:'), sg.Input(key='-KEY-')],
+          [sg.Multiline(visible=False, key='-DISPLAY-', size=(None, 5))],
+          [sg.Button('Open', key='-OPEN-'), sg.Button('Close'), sg.Button('Save', visible=False, key='-SAVE-')]]
 
-layout = [[sg.Text('Select a file:'), sg.Input(key='-INPUT-'), sg.FileBrowse()], [sg.Text('Encryption key:'), sg.Input(key='-KEY-')],[sg.Multiline(visible=False, key='-DISPLAY-')],[sg.Button('Open', key='-OPEN-'), sg.Button('Save', visible=False, key='-SAVE-'), sg.Cancel()]]
 
-window = sg.Window('File Encrypter').Layout(layout)
+screen_size = GetSystemMetrics(0) # get the computer width size
+scale_factor = screen_size / 1920 + 0.3 # get window scaling factor with the computer width size
 
+window = sg.Window('File Encrypter', layout, resizable=True, element_justification='center', scaling=scale_factor, finalize=True)
+
+window_state_prev = window.TKroot.state()
 while True:
     event, values = window.Read(timeout=100)
-    if event is None or event in ['Cancel', sg.WIN_CLOSED]:
+    if event is None or event in ['Close', sg.WIN_CLOSED]:
         break
-    elif event == '-OPEN-':
+    elif event == '-OPEN-' and isfile(values['-INPUT-']):
         cipher = AESCipher(values['-KEY-'])
         # This is checking if the file is empty. If it is, then it will encrypt the blank file so it can be decrypted after.
-        if not len(open(values['-INPUT-'], 'r', encoding='utf-8').read()):
-            encrypt_file(values['-INPUT-'], '', values['-KEY-'])
-        text = cipher.decrypt(open(values['-INPUT-'], 'r', encoding='utf-8').read())
-        window['-DISPLAY-'].update(text, visible=True)
-        window['-SAVE-'].update(visible=True)
+        try:
+            if not len(open(values['-INPUT-'], 'r', encoding='utf-8').read()):
+                encrypt_file(values['-INPUT-'], '', values['-KEY-'])
+        except UnicodeDecodeError:
+            sg.popup_error('The file is not in UTF-8 format or is corrupted!', title='UnicodeDecodeError')
+        try:
+            text = cipher.decrypt(open(values['-INPUT-'], 'r', encoding='utf-8').read())
+            window['-DISPLAY-'].update(text, visible=True)
+            window['-SAVE-'].update(visible=True)
+        except UnicodeDecodeError:
+            sg.popup_error('Key is probably incorrect!', title='UnicodeDecodeError')
     elif event == '-SAVE-':
         encrypt_file(values['-INPUT-'], values['-DISPLAY-'], values['-KEY-'])
         window['-SAVE-'].update(visible=False)
         window['-DISPLAY-'].update(visible=False)
+
+    window_state = window.TKroot.state()
+    if window_state_prev == "normal" and window_state == "zoomed":
+        window['-DISPLAY-'].set_size(size=(None, 55))
+        window_state_prev = "zoomed"
+    elif window_state_prev == "zoomed" and window_state == "normal":
+        window['-DISPLAY-'].set_size(size=(None, 5))
+        window_state_prev = "normal"
+window.Close()
